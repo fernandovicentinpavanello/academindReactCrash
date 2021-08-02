2d38500fcb122b2bf1e1bcf2a7c8f8a807c18d28
Luiz Vicentin <luizpavanello@gmail.com>
2021-08-02 18:46:37 -0300

    Deleting unused files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="App-header">
+        <h1>Hello World</h1>
+      </div>
     </div>
   );
 }
