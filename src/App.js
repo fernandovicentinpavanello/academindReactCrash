2d774860747801fae9b38f79aad3430b1d9a8ae6
Luiz Vicentin <luizpavanello@gmail.com>
2021-08-03 17:19:23 -0300

    Add a new layout on the page using Props Children

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupsPage from './pages/NewMeetups';
 import FavoritesPage from './pages/Favorites';
-import MainNavigation from './components/layout/MainNavigation'
+import Layout from './components/layout/Layout';
 
 function App() {
   return (
     <>
-      <MainNavigation />    
-      <Switch>
-        <Route path='/' exact component={AllMeetupsPage} />
-        <Route path='/new-meetups' component={NewMeetupsPage} />
-        <Route path='/favorites' component={FavoritesPage} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={AllMeetupsPage} />
+          <Route path="/new-meetups" component={NewMeetupsPage} />
+          <Route path="/favorites" component={FavoritesPage} />
+        </Switch>
+      </Layout>
     </>
   );
 }
