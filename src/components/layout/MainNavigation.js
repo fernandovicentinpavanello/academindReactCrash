e7e2409063a32aa6a1b62134b2213525e0b72ed1
Luiz Vicentin <luizpavanello@gmail.com>
2021-08-03 22:16:33 -0300

    Add some function to the project to use the api, to show the number of fav meetups, ...

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,19 +1,32 @@
-import { Link } from 'react-router-dom'
-import styleClasses from './MainNavigation.module.css';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './MainNavigation.module.css';
+import FavoritesContext from '../../store/favorites-context';
 
 export default function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+
   return (
-    <header className={styleClasses.header}>
-      <div>
-        React Meetups
-      </div>
+    <header className={styles.header}>
+      <div className={styles.logo}>React Meetups</div>
       <nav>
         <ul>
-          <li><Link to='/'>All Meetups</Link></li>
-          <li><Link to='/new-meetups'>Add New Meetup</Link></li>
-          <li><Link to='/favorites'>My Favorites</Link></li>
+          <li>
+            <Link to="/">All Meetups</Link>
+          </li>
+          <li>
+            <Link to="/new-meetups">Add New Meetup</Link>
+          </li>
+          <li>
+            <Link to="/favorites">
+              My Favorites
+              <span className={styles.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
-  )
+  );
 }
